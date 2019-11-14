867079c42af656546dbbd353ea235be70fb5c88f
Martin Kagume <martiniriga@gmail.com>
2019-11-14 11:11:42 +0300

    View links uploaded by users available to public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,27 @@
 @section('content')
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            Satia Board Links
         </div>
+        <ul>
+        @forelse($links as $link)
 
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
+                <li>{{$link->user->name}}    {{$link->url}} </li>
+
+        @empty
+            <h4>No Links added yet</h4>
+            @endforelse
+        </ul>
+            <div class="links">
+                <a href="https://laravel.com/docs">Docs</a>
+                <a href="https://laracasts.com">Laracasts</a>
+                <a href="https://laravel-news.com">News</a>
+                <a href="https://blog.laravel.com">Blog</a>
+                <a href="https://nova.laravel.com">Nova</a>
+                <a href="https://forge.laravel.com">Forge</a>
+                <a href="https://vapor.laravel.com">Vapor</a>
+                <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
     </div>
 @endsection
 
